660a58c166ac2984550e77f4ad75ccd704bbeaa7
Carolyn <carito.ulfe@gmail.com>
2018-02-01 21:23:26 +0100

    Contacs: Basic files updated

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,35 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class ContactList extends Component {
-  render() {
-    const people = this.props.contacts
-
-    return <ol>
-      {people.map(person => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
-  }
-}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: 'Michael' },
-          { name: 'Ryan' },
-          { name: 'Tyler' }
-        ]}/>
-        <ContactList contacts={[
-          { name: 'Amanda' },
-          { name: 'Richard' },
-          { name: 'Geoff' }
-        ]}/>
+      <div>
+        Hello World
       </div>
-    );
+    )
   }
 }
 
