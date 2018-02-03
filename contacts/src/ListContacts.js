d8288780f6b943b3e3ce29c4076ea2c7e1adfc63
Carolyn <carito.ulfe@gmail.com>
2018-02-03 13:52:59 +0100

    adding states and update states with setState

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 function ListContacts(props) {
   return <ol className = "contact-list">
@@ -14,7 +14,7 @@ function ListContacts(props) {
             <p>{contact.name}</p>
             <p>{contact.email}</p>
           </div>
-          <button className="contact-remove">Remove</button>
+          <button className="contact-remove" onClick={()=>props.onDeleteContact(contact)}>Remove</button>
         </li>
       ))
     }
