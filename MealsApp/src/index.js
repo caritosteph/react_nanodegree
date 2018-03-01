1b6a309beb5c832b233a391b133eed7b009af2a4
Carolyn <carito.ulfe@gmail.com>
2018-03-01 22:56:59 +0100

    Adding Provider

--- MealsApp/src/index.js
+++ MealsApp/src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
@@ -5,6 +6,7 @@ import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import { createStore } from 'redux'
 import reducer from './reducers'
+import { Provider } from 'react-redux'
 
 const store = createStore(
   reducer,
@@ -12,7 +14,9 @@ const store = createStore(
 )
 
 ReactDOM.render(
-  <App store={store} />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
 registerServiceWorker()
