75c7c7a26772ff455efd541fb1529a5c045170d9
Carolyn <carito.ulfe@gmail.com>
2018-03-03 11:35:55 +0100

    Combine Reducers

--- MealsApp/src/actions/index.js
+++ MealsApp/src/actions/index.js
@@ -1,19 +1,19 @@
 export const ADD_RECIPE = 'ADD_RECIPE';
 export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR';
 
-export function addRecipe({ day, recipe, meal}) {
-    return {
-        type: ADD_RECIPE,
-        day,
-        recipe,
-        meal
-    }
+export function addRecipe ({ day, recipe, meal }) {
+  return {
+    type: ADD_RECIPE,
+    recipe,
+    day,
+    meal,
+  }
 }
 
-export function removeFromCalendar({ day, meal }) {
-    return {
-        type: ADD_RECIPE,
-        day,
-        meal
-    }
+export function removeFromCalendar ({ day, meal }) {
+  return {
+    type: REMOVE_FROM_CALENDAR,
+    day,
+    meal,
+  }
 }
--- MealsApp/src/reducers/index.js
+++ MealsApp/src/reducers/index.js
@@ -1,64 +1,83 @@
-import { ADD_RECIPE, REMOVE_FROM_CALENDAR } from '../actions';
+import {
+  ADD_RECIPE,
+  REMOVE_FROM_CALENDAR,
+} from '../actions'
+
+function food (state = {}, action) {
+  switch (action.type) {
+    case ADD_RECIPE :
+      const { recipe } = action
+
+      return {
+        ...state,
+        [recipe.label]: recipe,
+      }
+    default :
+      return state
+  }
+}
 
 const initialCalendarState = {
-        sunday: {
-            breakfast: null,
-            lunch: null,
-            dinner: null
-        },
-        monday: {
-            breakfast: null,
-            lunch: null,
-            dinner: null
-        },
-        tuesday: {
-           breakfast: null,
-           lunch: null,
-           dinner: null
-        },
-        wednesday: {
-            breakfast: null,
-            lunch: null,
-            dinner: null
-        },
-        thursday: {
-            breakfast: null,
-            lunch: null,
-            dinner: null
-        },
-        friday: {
-            breakfast: null,
-            lunch: null,
-            dinner: null
-        },
-        saturday: {
-            breakfast: null,
-            lunch: null,
-            dinner: null
-        }
-};
+  sunday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null,
+  },
+  monday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null,
+  },
+  tuesday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null,
+  },
+  wednesday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null,
+  },
+  thursday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null,
+  },
+  friday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null,
+  },
+  saturday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null,
+  },
+}
 
 function calendar (state = initialCalendarState, action) {
-    const { day, recipe, meal } = action;
+  const { day, recipe, meal } = action
 
-    switch (action.type) {
-        case ADD_RECIPE:
-            return {
-                ...state,
-                [day]: {
-                    ...state[day],
-                    [meal]: recipe.label
-                }
-            }
-        case REMOVE_FROM_CALENDAR:
-            return {
-                ...state,
-                [day]: {
-                    ...state,
-                    [meal]: null
-                }
-            }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case ADD_RECIPE :
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: recipe.label,
+        }
+      }
+    case REMOVE_FROM_CALENDAR :
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: null,
+        }
+      }
+    default :
+      return state
+  }
 }
+
+export default calendar;
