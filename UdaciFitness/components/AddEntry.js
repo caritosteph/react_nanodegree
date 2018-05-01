90b021856d4439a09b28901b8d0f9f96807484c9
Carolyn <carito.ulfe@gmail.com>
2018-05-02 01:15:25 +0200

    adding touchable buttons

--- UdaciFitness/components/AddEntry.js
+++ UdaciFitness/components/AddEntry.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { View, TouchableOpacity, Text } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciStepper from './UdaciStepper'
+import DateHeader from './DateHeader.js'
+
+function SubmitBtn ({ onPress }) {
+	return (
+		<TouchableOpacity
+			onPress={onPress}>
+			<Text>SUBMIT</Text>
+		</TouchableOpacity>
+	)
+}
 
 class AddEntry extends Component {
 	state = {
@@ -43,12 +53,34 @@ class AddEntry extends Component {
 		}))
 	}
 
+	submit = () => {
+		const key = timeToString()
+		const entry = this.state
+
+		// Update redux
+
+		this.setState(()=>({
+			run:  0,
+			bike: 0,
+			swim: 0,
+			sleep: 0,
+			eat: 0
+		}))
+
+		// Navigate  to home 
+
+		// Save to  'DB'
+
+		// Clearn local notification
+	}
+
   	render() {
 
   		const metaInfo = getMetricMetaInfo()
 
     	return (
     		<View>
+    			<DateHeader date={(new Date()).toLocaleDateString()} />
 				{Object.keys(metaInfo).map((key) => {
 					const { getIcon, type, ...rest } = metaInfo[key]
 					const value = this.state[key]
@@ -72,6 +104,7 @@ class AddEntry extends Component {
 						</View>
 					)
 				})}
+				<SubmitBtn onPress={this.submit}/>
     		</View>
     )
   }
