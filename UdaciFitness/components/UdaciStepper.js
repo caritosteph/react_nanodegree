045757e4da4dee25ff2d17191f6b24eedd01ad2e
Carolyn <carito.ulfe@gmail.com>
2018-05-01 16:47:23 +0200

    add udacity slider and udaciStepper

---
+++ UdaciFitness/components/UdaciStepper.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function UdaciStepper () {
+	return (
+		<View>
+			<Text>UdaciStepper</Text>
+		</View>
+	)
+}
